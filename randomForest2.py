ceb827c19c72a87a084907dbb71204b9927c94d4
Chandima Anuradha <canuradhah@gmail.com>
2019-10-24 15:15:39 +0530

    Testing of the model using a validation dataset

--- randomForest2.py
+++ randomForest2.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 ####### Dataset can be found at https://archive.ics.uci.edu/ml/datasets/Adult
 adult = pd.read_csv("../datasets/US income/adult.data", header=None)
-# adult_test = pd.read_csv("lrn/datasets/US income/adult.test", header=None)
+
 
 adult.columns = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation",
  "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "salary"]
@@ -85,16 +85,51 @@ fet_test = target_enc.transform(fet_test)
 
 estimators = [10, 20, 50, 100, 250, 500, 1000]
 
-for est in estimators:
-    print("Estimator: ", est)
-    for leaves in estimators:
+# for est in estimators:
+#     print("Estimator: ", est)
+#     for leaves in estimators:
 
-        rf_classifier = RandomForestClassifier(random_state= 0, n_estimators=est, max_leaf_nodes = leaves)
+#         rf_classifier = RandomForestClassifier(random_state= 0, n_estimators=est, max_leaf_nodes = leaves)
 
-        rf_classifier.fit(fet_train, tar_train)
+#         rf_classifier.fit(fet_train, tar_train)
 
-        predictions = rf_classifier.predict(fet_test)
+#         predictions = rf_classifier.predict(fet_test)
 
-        error = mean_absolute_error(predictions, tar_test)
+#         error = mean_absolute_error(predictions, tar_test)
 
-        print("Leaves : ",leaves, "  error: ",  error)
+#         print("Leaves : ",leaves, "  error: ",  error)
+
+rf_classifier = RandomForestClassifier(random_state= 0, n_estimators=500, max_leaf_nodes = 500)
+rf_classifier.fit(fet_train, tar_train)
+
+############# Validating the model
+
+adult_Validate = pd.read_csv("../datasets/US income/adult.test", header=None)
+adult_Validate.columns = adult.columns
+valid_feat = adult_Validate.drop(['education', 'salary'], axis = 1)
+valid_target = adult_Validate['salary'].to_frame()
+valid_target = valid_target.replace([' <=50K.', ' >50K.'], [0, 1])
+valid_target = valid_target['salary']
+# print(adult_Validate.head())
+
+##### Encoding categorical data in the validation set
+
+##Label Encoding
+valid_label_enc =  valid_feat[lbl_encoded_fet].apply(lambda x: encoder[x.name].transform(x))
+valid_feat = pd.concat([valid_feat.drop(lbl_encoded_fet, axis = 1), valid_label_enc], axis = 1)
+
+## One hot encoding
+dummy_valid = pd.get_dummies(valid_feat['relationship'], prefix = 'rel_')
+dummy_valid.reindex(columns = dummy_train.columns, fill_value = 0)
+valid_feat = valid_feat.drop(['relationship'], axis = 1).join(dummy_valid)
+
+## Target Encoding
+valid_feat = target_enc.transform(valid_feat)
+
+# #####valid predictions
+
+valid_predict = rf_classifier.predict(valid_feat)
+
+valid_error = mean_absolute_error(valid_predict, valid_target)
+
+print("error: ", valid_error)
