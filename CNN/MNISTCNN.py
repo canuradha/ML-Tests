f27c237a4018dc79fa25dd43e02a67c307dd9478
Chandima Anuradha <canuradhah@gmail.com>
2019-11-29 07:43:04 +0530

    data added from buffer to a numpy array

--- CNN/MNISTCNN.py
+++ CNN/MNISTCNN.py
@@ -3,22 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import struct
 
-# all the necessary data to read the data file is given at http://yann.lecun.com/exdb/mnist/
+# -----------------all the necessary data to read the data file is given at http://yann.lecun.com/exdb/mnist/
 Train_Data = open('../../Data/MNIST/train-images-idx3-ubyte', 'rb')
+Train_Labels = open('../../Data/MNIST/train-labels-idx1-ubyte', 'rb')
 
-# Skip the magic number
+# -----------------Skip the magic number
 Train_Data.read(4)
+Train_Labels.read(4)
 
-# get Dimensions
+# -----------------Get Dimensions
 imageCount = struct.unpack('>i', Train_Data.read(4))[0]
-imageSize =  struct.unpack('>i', Train_Data.read(4))[0] * struct.unpack('>i', Train_Data.read(4))[0] 
+imageSize =  [struct.unpack('>i', Train_Data.read(4))[0], struct.unpack('>i', Train_Data.read(4))[0]]
 
-first_image = np.frombuffer(Train_Data.read(imageSize), dtype=np.uint8)
-first_image = first_image.reshape(28, 28)
+LabelCount = struct.unpack('>i', Train_Labels.read(4))[0]
 
-print(first_image)
+# -----------------Create a numpy array with all the images
+Train_X = np.zeros([imageCount, imageSize[0], imageSize[1]], dtype=float)
 
-plt.imshow(first_image)
-plt.show()
+# print(np.prod(imageSize))
+
+for i in range(imageCount):
+    Train_X[i] = np.frombuffer(Train_Data.read(np.prod(imageSize)), dtype=np.uint8).reshape(imageSize)
+
+# first_image = np.frombuffer(Train_Data.read(28* 28), dtype=np.uint8).reshape(28, 28)
+
+Labels = np.frombuffer(Train_Labels.read(), dtype=np.uint8)
+
+
+# plt.imshow(Train_X[0])
+# plt.show()
 
 
